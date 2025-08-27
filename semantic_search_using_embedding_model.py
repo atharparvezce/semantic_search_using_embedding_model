594a40a55023c1de652321d196e944a72f534ab8
Athar Parvez <atharparvezce@gmail.com>
2025-08-27 13:04:14 +0300

    adding

--- semantic_search_using_embedding_model.py
+++ semantic_search_using_embedding_model.py
@@ -19,17 +19,21 @@ documents = [
     "Jasprit Bumrah is an Indian fast bowler known for his unorthodox action and yorkers."
 ]
 
-query = "Tell me about Virat Kohli"
+query = "Tell me about Jasprit"
 
 doc_embeddings = embedding.embed_documents(documents)
 query_embeddings=  embedding.embed_query(query)
 
-# print(cosine_similarity([query_embeddings], doc_embeddings))
+#print(cosine_similarity([query_embeddings], doc_embeddings))
 
-scores = cosine_similarity([query_embeddings], doc_embeddings)
+scores = cosine_similarity([query_embeddings], doc_embeddings)[0]
 
-#print(list(enumerate(score)))
+#print(list(enumerate(scores)))
 #print(sorted(list(enumerate(scores)), key = lambda x:x[1]))
+#print(sorted(list(enumerate(scores)), key = lambda x:x[1])[-1])
 
-print(sorted(list(enumerate(scores)), key = lambda x:x[1])[-1])
+index , score = sorted(list(enumerate(scores)), key = lambda x:x[1])[-1]
 
+print(query)
+print(documents[index])
+print("Similarity Score is:", score)
